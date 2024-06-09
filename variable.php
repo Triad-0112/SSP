f8b05a5cd457ef24e41b4f1b3b8301e261d5be13
Panji Tri Wahyudi <richard.kennedy11316@gmail.com>
2024-06-09 09:26:48 +0700

    test

---
+++ variable.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Document</title>
+    </head>
+</html>
+<body>
+    <?php
+        $nama="Panji";
+        //$kelas=12;
+        //$jurusan="TKJ";
+        define("Gaji", "5000000");
+        //echo Cetak, "<br>";
+        echo "Karyawan yang bernama $nama sekarang memiliki penghasilan ".Gaji;
+    ?>
+</body>
