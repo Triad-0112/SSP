6d5c46f0590bdffe75c0f305bb62066adedcae09
Panji Tri Wahyudi <richard.kennedy11316@gmail.com>
2024-06-09 09:48:32 +0700

    tanggal 9 juni 2024

--- variable.php
+++ variable.php
@@ -8,11 +8,19 @@
 </html>
 <body>
     <?php
-        $nama="Panji";
+        //$nama="Panji";
         //$kelas=12;
         //$jurusan="TKJ";
-        define("Gaji", "5000000");
+        //define("Gaji", "5000000");
+        $integer =10;
+        $string = "Hello World";
+        $float = 1.4;
+        //$array=array("1,"string",1.4,2.5,5,6,7,8,9,10,11,12,13");
+        $siswa=array("Panji","Alux","Saturn","Venus");
+        echo"</br>";
+        echo "$siswa[0],$siswa[1],$siswa[2],$siswa[3]";
+        //var_dump($integer,$string,$float,$array);
         //echo Cetak, "<br>";
-        echo "Karyawan yang bernama $nama sekarang memiliki penghasilan ".Gaji;
+        //echo "Karyawan yang bernama $nama sekarang memiliki penghasilan ".Gaji;
     ?>
 </body>
