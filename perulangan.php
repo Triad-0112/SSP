43f81c4169d5862c6e05a38cf7f5641b170c3cb2
Panji Tri Wahyudi <richard.kennedy11316@gmail.com>
2024-06-09 10:02:20 +0700

    tanggal 9 juni 2024

--- perulangan.php
+++ perulangan.php
@@ -8,6 +8,13 @@
 </html>
 <body>
     <?php
-        
+        $user="user";
+        if($user=="admin"){
+            echo "Selamat Datang Admin!!";
+        } else if ($user=="user"){
+            echo "Selain Datang User!!!";
+        } else {
+            echo "Maaf, saya tidak kenal anda";
+        }
     ?>
 </body>
---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Document</title>
+    </head>
+</html>
+<body>
+    <?php
+        $a=25;
+        if($a%2==0){
+            echo"$a adalah angka genap";
+        } else{
+        echo"$a adalah angka ganjil";
+        }
+    ?>
+</body>
